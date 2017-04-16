d503b8beb274aab72290429fb1cf9ad4583ea1d8
Marek Kamiński <marek.kaminski.93@gmail.com>
2017-04-16 14:12:32 +0200

    Improved appearance

--- PS/ViewModel/BaseViewModel.cs
+++ PS/ViewModel/BaseViewModel.cs
@@ -9,21 +9,6 @@ namespace PS.ViewModel
 {
     public abstract class BaseViewModel : ViewModelBase {
         private readonly MetroWindow _windowsInstance = Application.Current.MainWindow as MetroWindow;
-        private ViewModelBase _currentViewModel;
-
-        public ViewModelBase CurrentViewModel {
-            get { return _currentViewModel; }
-            set {
-                if (_currentViewModel == value)
-                    return;
-                _currentViewModel = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public virtual void NavigateTo(ViewModelBase viewModel) {
-            CurrentViewModel = viewModel;
-        }
 
         public async void DisplayDialog(string title, string description) {
             await _windowsInstance.ShowMessageAsync(title, description);
--- PS/ViewModel/Windows/MainViewModel.cs
+++ PS/ViewModel/Windows/MainViewModel.cs
@@ -1,21 +1,72 @@
-using GalaSoft.MvvmLight.Command;
+﻿using MahApps.Metro.Controls;
 
 namespace PS.ViewModel.Windows {
     public sealed class MainViewModel : BaseViewModel {
-        private RelayCommand _openLab1Page;
-        private RelayCommand _openLab2Page;
-        private RelayCommand _openLab3Page;
-        private RelayCommand _openLab4Page;
-        private RelayCommand _openLab5Page;
+        private HamburgerMenuGlyphItem _selectedMenuItem;
+        private HamburgerMenuItemCollection _menuItems;
+
+        private readonly HamburgerMenuGlyphItem _lab1 = new HamburgerMenuGlyphItem
+        {
+            Glyph = "",
+            Label = "Lab1 - base64",
+            Tag = ViewModelLocator.Instance.Lab1
+        };
+        private readonly HamburgerMenuGlyphItem _lab2 = new HamburgerMenuGlyphItem
+        {
+            Glyph = "",
+            Label = "Lab2 - POP3",
+            Tag = ViewModelLocator.Instance.Lab2
+        };
+        private readonly HamburgerMenuGlyphItem _lab3 = new HamburgerMenuGlyphItem
+        {
+            Glyph = "",
+            Label = "Lab3 - SMTP",
+            Tag = ViewModelLocator.Instance.Lab3
+        };
+        private readonly HamburgerMenuGlyphItem _lab4 = new HamburgerMenuGlyphItem
+        {
+            Glyph = "",
+            Label = "Lab4 - FTP",
+            Tag = ViewModelLocator.Instance.Lab4
+        };
+        private readonly HamburgerMenuGlyphItem _lab5 = new HamburgerMenuGlyphItem
+        {
+            Glyph = "",
+            Label = "Lab5 - HTTP Crawler",
+            Tag = ViewModelLocator.Instance.Lab5
+        };
+
 
         public MainViewModel() {
-            NavigateTo(ViewModelLocator.Instance.Lab1);
+            MenuItems = new HamburgerMenuItemCollection {
+                _lab1,
+                _lab2,
+                _lab3,
+                _lab4,
+                _lab5
+            };
+            SelectedMenuItem = _lab1; // Default view after app run
         }
 
-        public RelayCommand OpenLab1Page => _openLab1Page ?? (_openLab1Page = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Lab1)));
-        public RelayCommand OpenLab2Page => _openLab2Page ?? (_openLab2Page = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Lab2)));
-        public RelayCommand OpenLab3Page => _openLab3Page ?? (_openLab3Page = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Lab3)));
-        public RelayCommand OpenLab4Page => _openLab4Page ?? (_openLab4Page = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Lab4)));
-        public RelayCommand OpenLab5Page => _openLab5Page ?? (_openLab5Page = new RelayCommand(() => NavigateTo(ViewModelLocator.Instance.Lab5)));
+        public HamburgerMenuGlyphItem SelectedMenuItem {
+            get => _selectedMenuItem;
+            set {
+                if (Equals(_selectedMenuItem, value))
+                    return;
+
+                _selectedMenuItem = value;
+                RaisePropertyChanged();
+            }
+        }
+        public HamburgerMenuItemCollection MenuItems {
+            get => _menuItems;
+            set {
+                if (Equals(_menuItems, value))
+                    return;
+
+                _menuItems = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
